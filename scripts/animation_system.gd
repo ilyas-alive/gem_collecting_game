471dcbc139eb6a866c90cc4cb835412938755196
Ilyas El Ghandouri <iel-ghan@c3r3p5.1337.ma>
2026-06-23 12:56:51 +0100

    finished player and tilemap

---
+++ scripts/animation_system.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var player_controller : Playercontroller
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _process(delta: float) -> void:
+	sprite_2d.flip_h = false
+	
+	if player_controller.velocity.length() > 0.0:
+		if player_controller.player_facing == player_controller.Facing.UP:
+			animation_player.play("move_up")
+		elif player_controller.player_facing == player_controller.Facing.DOWN:
+			animation_player.play("move_down")
+		elif player_controller.player_facing == player_controller.Facing.LEFT:
+			animation_player.play("move_left")
+		elif player_controller.player_facing == player_controller.Facing.RIGHT:
+			animation_player.play("move_left")
+			sprite_2d.flip_h = true
+	else:
+		if player_controller.player_facing == player_controller.Facing.UP:
+			animation_player.play("idle_up")
+		elif player_controller.player_facing == player_controller.Facing.DOWN:
+			animation_player.play("idle_down")
+		elif player_controller.player_facing == player_controller.Facing.LEFT:
+			animation_player.play("idle_left")
+		elif player_controller.player_facing == player_controller.Facing.RIGHT:
+			animation_player.play("idle_left")
+			sprite_2d.flip_h = true
---
+++ scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+class_name Playercontroller
+
+@export var move_speed = 20.0
+@export var sprint_increase = 2
+
+var direction : Vector2
+var sprinting = false
+var sprint_multiplier = 1.0
+
+enum Facing {UP, DOWN, LEFT, RIGHT}
+var player_facing : Facing
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_down"):
+		direction.y = 1
+		player_facing = Facing.DOWN
+		
+	elif Input.is_action_pressed("move_up"):
+		direction.y = -1
+		player_facing = Facing.UP
+	else:
+		direction.y = 0
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+		player_facing = Facing.LEFT
+		
+	elif Input.is_action_pressed("move_right"):
+		direction.x = 1
+		player_facing = Facing.RIGHT
+		
+	else:
+		direction.x = 0
+	if Input.is_action_pressed("sprint"):
+		sprint_multiplier = sprint_increase
+		sprinting = true
+	else:
+		sprint_multiplier = 1.0
+		sprinting = false
+	velocity = delta * move_speed * direction.normalized() * 200 * sprint_multiplier
+	move_and_slide()
